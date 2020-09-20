87d899f913fdc046e5c04772db09cbfab55fc566
Marc-Antoine-K <marc.antoine.hubert.pro@gmail.com>
2020-09-20 14:59:16 +0800

    cyclic rotation

---
+++ BinaryGap.js
@@ -0,0 +1,26 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const NBin = N.toString(2);
+    let counting = false;
+    let answer = 0;
+    let tmp = 0;
+
+    for (let i = 0; i < NBin.length; i++){
+        if (!counting && NBin[i] === "1"){
+            counting = true;
+        }
+        else if (counting && NBin[i] === "0") {
+            tmp++;
+        }
+        else{
+            if (tmp > answer){
+                answer = tmp;
+            }
+            tmp = 0;
+        }
+    }
+    return answer;
+}
---
+++ CyclicRotation.js
@@ -0,0 +1,22 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const A.length = l;
+    const Kr= K%l;  
+    if (Kr = 0){
+        return A;
+    }
+    else{
+        const RotatedA = [...A];
+        for(let i = 0; i < l; i++){
+            if (i + Kr < l){
+                RotatedA[i + Kr] = A[i];
+            }
+            else{
+                RotatedA[i + Kr -l] = A[i];
+            }
+        }
+    }
+}
