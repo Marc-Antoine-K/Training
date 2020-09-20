7f9191ba76b847831be62e4f92ffc7a7b436439f
Marc-Antoine-K <marc.antoine.hubert.pro@gmail.com>
2020-09-20 15:17:37 +0800

    Cylic Rotation

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -21,3 +21,9 @@ function solution(A, K) {
         return RotatedA;
     }
 }
+
+/* function solution(A, K) {
+    K = K % A.length;
+    const sliceIndex = A.length - K;
+    return [...A.slice(sliceIndex), ...A.slice(0, sliceIndex)]
+ } */
