9ba9aaf4a42467862c7d6c7f1a58c8f34f28b6be
Marc-Antoine-K <marc.antoine.hubert.pro@gmail.com>
2020-09-21 17:28:44 +0800

    MissingElement

---
+++ MissingElement.js
@@ -0,0 +1,25 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const N = A.length;
+    let sum = A.reduce((sum,el) => {
+        sum += el;
+        return parseInt(sum);
+    },0);
+
+    let result = (N+1)*(N+2)/2 - sum;
+    return result;
+}
+
+/* function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const N = A.length;
+    let sum = A.reduce((sum,el) => {
+        return parseInt(sum + el);
+    },0);
+
+    let result = (N+1)*(N+2)/2 - sum;
+    return result;
+} */
