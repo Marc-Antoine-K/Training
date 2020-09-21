1f672177709c4bcc4f5027d747f4d125117169f8
Marc-Antoine-K <marc.antoine.hubert.pro@gmail.com>
2020-09-21 15:34:30 +0800

    OddOcurrencesInArray.js

---
+++ OddOccurrencesInArray.js
@@ -0,0 +1,32 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  let map = A.reduce((map, el) => {
+    map[el] = ++map[el] || 1;
+    return map;
+  }, {});
+  let result = Object.keys(map).filter((el) => map[el] % 2 !== 0);
+  return +result[0];
+}
+
+/* function solution(A) {
+  let map = A.reduce((map, el) => {
+    if (map[el] === undefined) {
+      map[el] = 1;
+    } else {
+      map[el]++;
+    }
+    return map;
+  }, {});
+
+  function result(map) {
+    for (properties in map) {
+      if (map[properties] % 2 !== 0) {
+        return parseInt(properties);
+      }
+    }
+  }
+  return result(map);
+} */
