684a18e0e31742b55ebaad88a524daf03d12e855
Marc-Antoine-K <marc.antoine.hubert.pro@gmail.com>
2020-10-01 15:34:33 +0800

    MissingElement

--- MissingElement.js
+++ MissingElement.js
@@ -16,10 +16,7 @@ function solution(A) {
 /* function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
     const N = A.length;
-    let sum = A.reduce((sum,el) => {
-        return parseInt(sum + el);
-    },0);
+    let sum = A.reduce((sum,el) => sum + el,0);
 
-    let result = (N+1)*(N+2)/2 - sum;
-    return result;
+    return (N+1)*(N+2)/2 - sum;
 } */
